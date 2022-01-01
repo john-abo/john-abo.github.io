1067aeb72b22547320567765d1de22b64f205f8f
john-abo <john.abo@ucalgary.ca>
2022-01-01 07:24:52 -0700

    Added some more content
    Added some text for projects as well as tabs for both types of projects

--- script/scripting.js
+++ script/scripting.js
@@ -5,3 +5,14 @@ function closeNav() {
 function openNav() {
     document.getElementById("sideMenu").style.width = "250px"
 }
+
+function showtab(tab) {
+    var i
+    var tabs = document.getElementsByClassName("project_tab")
+
+    for (i = 0; i < tabs.length; i++) {
+        tabs.item(i).style.display = "none"
+    }
+
+    document.getElementById(tab).style.display = "block"
+}
